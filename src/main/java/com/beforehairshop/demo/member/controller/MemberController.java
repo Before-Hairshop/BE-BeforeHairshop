e07ffc0c388531c5efdee3f87948da05c3f6eb0b
bohub12 <qhrb1997@naver.com>
2022-09-12 21:28:15 +0900

    feature. 유저 프로필 조회, 생성, 수정 API

--- src/main/java/com/beforehairshop/demo/member/controller/MemberController.java
+++ src/main/java/com/beforehairshop/demo/member/controller/MemberController.java
@@ -1,6 +1,8 @@
 package com.beforehairshop.demo.member.controller;
 
 import com.beforehairshop.demo.auth.PrincipalDetails;
+import com.beforehairshop.demo.aws.S3Uploader;
+import com.beforehairshop.demo.member.dto.MemberProfilePatchRequestDto;
 import com.beforehairshop.demo.member.dto.MemberProfileSaveRequestDto;
 import com.beforehairshop.demo.member.service.MemberService;
 import com.beforehairshop.demo.response.ResultDto;
@@ -12,6 +14,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 import static com.beforehairshop.demo.response.ResultDto.*;
 
@@ -22,29 +27,20 @@ import static com.beforehairshop.demo.response.ResultDto.*;
 public class MemberController {
 
     private final MemberService memberService;
+    private final S3Uploader s3Uploader;
 
     @PreAuthorize("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')")
-    @GetMapping("test")
-    public ResponseEntity<ResultDto> testFind(@AuthenticationPrincipal PrincipalDetails principalDetails) {
+    @GetMapping("")
+    public ResponseEntity<ResultDto> findMe(@AuthenticationPrincipal PrincipalDetails principalDetails) {
         return makeResult(HttpStatus.OK, principalDetails.getMember());
     }
 
-    @PreAuthorize("hasAnyRole('ROLE_USER', 'ROLE_ADMIN')")
-    @GetMapping("")
-    public ResponseEntity<ResultDto> findMyProfile(@AuthenticationPrincipal PrincipalDetails principalDetails) {
-        return memberService.findMyProfile(principalDetails.getMember());
-    }
+//    @Tag(name = "유저 이미지 수정 API", description = "유저나 헤어 디자이너 모두 이미지를 바꿀 때 사용하는 API")
+    @PreAuthorize("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')")
+    @PatchMapping("/image")
+    public ResponseEntity<ResultDto> patchMyImage(@AuthenticationPrincipal PrincipalDetails principalDetails, @RequestBody MultipartFile myImageFile) throws IOException {
 
-    @PreAuthorize("hasAnyRole('ROLE_USER', 'ROLE_ADMIN')")
-    @PostMapping("")
-    public ResponseEntity<ResultDto> saveMemberProfile(@AuthenticationPrincipal PrincipalDetails principalDetails, @RequestBody MemberProfileSaveRequestDto memberProfileSaveRequestDto) {
-        return memberService.saveMemberProfile(principalDetails.getMember(), memberProfileSaveRequestDto);
+        return makeResult(HttpStatus.OK, s3Uploader.upload(myImageFile, principalDetails.getMember().getId().toString() + "/profile.jpg"));
     }
-
-//    @PostMapping("signup-google")
-//    @Operation(summary = "구글 이메일로 가입")
-//    public ResponseEntity<ResultDto> signUpGoogle (@RequestBody MemberSaveDto memberSaveDto) {
-//        return memberService.signUpGoogle(memberSaveDto);
-//    }
 
 }
---
+++ src/main/java/com/beforehairshop/demo/member/controller/MemberProfileController.java
@@ -0,0 +1,48 @@
+package com.beforehairshop.demo.member.controller;
+
+import com.beforehairshop.demo.auth.PrincipalDetails;
+import com.beforehairshop.demo.aws.S3Uploader;
+import com.beforehairshop.demo.member.dto.MemberProfilePatchRequestDto;
+import com.beforehairshop.demo.member.dto.MemberProfileSaveRequestDto;
+import com.beforehairshop.demo.member.service.MemberService;
+import com.beforehairshop.demo.response.ResultDto;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+
+@RestController
+@Tag(name = "유저 프로필 관련 Controller")
+@AllArgsConstructor
+@RequestMapping("/api/v1/members/profiles")
+public class MemberProfileController {
+    private final MemberService memberService;
+    private final S3Uploader s3Uploader;
+
+
+    @PreAuthorize("hasAnyRole('ROLE_USER', 'ROLE_ADMIN')")
+    @GetMapping("")
+    public ResponseEntity<ResultDto> findMyProfile(@AuthenticationPrincipal PrincipalDetails principalDetails) {
+        return memberService.findMyProfile(principalDetails.getMember());
+    }
+
+    @PreAuthorize("hasAnyRole('ROLE_USER', 'ROLE_ADMIN')")
+    @PostMapping("")
+    public ResponseEntity<ResultDto> saveMemberProfile(@AuthenticationPrincipal PrincipalDetails principalDetails
+            , MemberProfileSaveRequestDto memberProfileSaveRequestDto) throws IOException {
+
+        return memberService.saveMemberProfile(principalDetails.getMember(), memberProfileSaveRequestDto, s3Uploader);
+    }
+
+
+    @PreAuthorize("hasAnyRole('ROLE_USER', 'ROLE_ADMIN')")
+    @PatchMapping("")
+    public ResponseEntity<ResultDto> patchMyProfile(@AuthenticationPrincipal PrincipalDetails principalDetails
+            , MemberProfilePatchRequestDto memberProfilePatchRequestDto) throws IOException {
+        return memberService.patchMyProfile(principalDetails.getMember(), memberProfilePatchRequestDto, s3Uploader);
+    }
+}

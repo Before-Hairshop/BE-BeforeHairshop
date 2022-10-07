9cbe4347d09a06fa45de962a9673799f95eae5a9
bohub12 <qhrb1997@naver.com>
2022-10-07 13:35:08 +0900

    fix. dev, prod 환경에서는 redis 연결 관련 에러는 체크하지 않도록 수정

--- src/test/java/com/beforehairshop/demo/DemoApplicationTests.java
+++ src/test/java/com/beforehairshop/demo/DemoApplicationTests.java
@@ -1,9 +1,14 @@
 package com.beforehairshop.demo;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@EnabledIfEnvironmentVariable(
+		named = "SPRING_PROFILES_ACTIVE",
+		matches = "local"
+)
 class DemoApplicationTests {
 
 	@Test
